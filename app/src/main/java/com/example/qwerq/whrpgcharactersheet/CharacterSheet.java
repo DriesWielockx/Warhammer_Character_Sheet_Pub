495afd4b49e247ca040eded9bcf246a89e19174d
Dries Wielockx <dries.wielockx@student.uhasselt.be>
2018-04-14 23:36:11 +0200

    Added working with and switching between fragments to character sheet

--- app/src/main/java/com/example/qwerq/whrpgcharactersheet/CharacterSheet.java
+++ app/src/main/java/com/example/qwerq/whrpgcharactersheet/CharacterSheet.java
@@ -1,29 +1,31 @@
 package com.example.qwerq.whrpgcharactersheet;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class CharacterSheet extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        CharacterSheetCombat.OnFragmentInteractionListener,
+        CharacterSheetOverview.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_character_sheet);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -35,6 +37,17 @@ public class CharacterSheet extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        CharacterSheetOverview fragTop = new CharacterSheetOverview();
+
+        FragmentTransaction ft = fm.beginTransaction();
+
+        //ft.add(android.R.id.content, fragTop);
+        ft.add(R.id.test, fragTop);
+
+        ft.commit();
     }
 
     @Override
@@ -45,21 +58,6 @@ public class CharacterSheet extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -79,11 +77,22 @@ public class CharacterSheet extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+            CharacterSheetCombat fragment = new CharacterSheetCombat();
 
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction transaction = fm.beginTransaction();
+            //transaction.replace(android.R.id.content, fragment);
+            transaction.replace(R.id.test, fragment);
+            transaction.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
